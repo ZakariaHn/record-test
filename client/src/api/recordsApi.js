c61f64edb6416801afb6aba76a9a83018fbf275c
zakariahn <zakaria@posteo.net>
2024-10-09 15:21:41 +0200

    update

--- client/src/api/recordsApi.js
+++ client/src/api/recordsApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllRecords = async (recordsDispatch) => {
   try {
-    const response = await axios.get("http://localhost:8000/records");
+    const response = await axios.get("/records");
 
     recordsDispatch({ type: "FETCH_RECORDS_SUCCESS", payload: response.data });
   } catch (error) {
